699365e9ea3eb400584fae4b89a1741fb56919f0
abhi12aw <58732462+abhi12aw@users.noreply.github.com>
2021-04-11 21:59:43 +0530

    added new navigation animation

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@ const openBtn = document.querySelector('button[data-btnOpen]');
 const closeBtn = document.querySelector('button[data-btnClose]');
 const nav = document.querySelector('div[data-nav]');
 
-function triggerNav()  {
- nav.classList.toggle('hidden');
-}
-closeBtn.addEventListener('click', triggerNav);
-openBtn.addEventListener('click', triggerNav);
+closeBtn.addEventListener('click', function()  {
+    nav.style.opacity = 0;
+    setTimeout(function()  {
+     nav.classList.add('hidden');
+    }, 150)
+});
+openBtn.addEventListener('click', function() {
+    nav.classList.remove('hidden');
+    setTimeout(function()  {
+        nav.style.opacity = 100;
+
+    }, 20)
+});
