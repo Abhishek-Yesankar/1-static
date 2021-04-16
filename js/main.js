6ea9a65dfb8e85ad4965fea4bdf29dd75ef35b6a
abhi12aw <58732462+abhi12aw@users.noreply.github.com>
2021-04-16 20:04:31 +0530

    added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const openBtn = document.querySelector('button[data-btnOpen]');
-const closeBtn = document.querySelector('button[data-btnClose]');
-const nav = document.querySelector('div[data-nav]');
-
+const openBtn = document.querySelector('button[data-btnOpen]'), closeBtn = document.querySelector('button[data-btnClose]'), 
+ nav = document.querySelector('div[data-nav]'), scrollToFeatureBtn = document.querySelector('a[data-scroll-to-feature]'),
+ scrollToFeatureBtnM = document.querySelector('a[data-scroll-to-feature-m]'),scrollToPricingBtnM = document.querySelector('a[data-scroll-to-pricing-m]')
+ toScrollFeature = document.querySelector('#feature'), scrollToPricingBtn = document.querySelector('a[data-scroll-to-pricing]'),
+ toScrollPricing = document.querySelector('#pricing');
 closeBtn.addEventListener('click', function()  {
     nav.style.opacity = 0;
     setTimeout(function()  {
@@ -12,6 +13,20 @@ openBtn.addEventListener('click', function() {
     nav.classList.remove('hidden');
     setTimeout(function()  {
         nav.style.opacity = 100;
-
     }, 20)
 });
+///smooth scrolling function
+function smoothScroll(toScroll, event)  {
+  event.preventDefault();
+  let cord = toScroll.getBoundingClientRect();
+  window.scrollTo({
+      left: cord.left + window.pageXOffset,
+      top: cord.top + window.pageYOffset,
+      behavior: 'smooth',
+  })
+}
+///smooth scrolling events
+scrollToFeatureBtn.addEventListener('click', smoothScroll.bind(null,toScrollFeature));
+scrollToPricingBtn.addEventListener('click', smoothScroll.bind(null,toScrollPricing));
+scrollToFeatureBtnM.addEventListener('click', smoothScroll.bind(null,toScrollFeature));
+scrollToPricingBtnM.addEventListener('click', smoothScroll.bind(null,toScrollPricing));
